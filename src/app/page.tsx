01ed8b1997878ecb640061bab37b1d0d2355c18f
hongnguyen1508 <hongnguyenarmy@gmail.com>
2023-08-04 20:12:04 +0700

    test page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
+import React from 'react'
 
-export default function Home() {
+const Home: React.FC = () => {
+  let myName: string = 'Rosa'
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -8,6 +10,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
+        <p>This page is {myName}</p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -111,3 +114,5 @@ export default function Home() {
     </main>
   )
 }
+
+export default Home
